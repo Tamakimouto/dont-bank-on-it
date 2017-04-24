6a53335cc88f10b6a4d9333845b6c20035865582
Anthony Zheng <Anthony@fopen-dream.space>
2017-04-24 17:13:03 -0400

    update db install

--- db/bonit.sql
+++ db/bonit.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `branches`
 --
 
+CREATE DATABASE bonit;
+USE bonit;
+
 DROP TABLE IF EXISTS `branches`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
